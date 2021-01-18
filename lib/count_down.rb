5095df78a26e7adf7f54cae41a229504dcedc337
Roman Turner <rn.turner@live.com>
2021-01-18 18:28:30 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,6 +1,8 @@
 #This will countdown from 10 to 1, and then it will print "Happy New Year!"
 
 count = 1
-while count >= count do
-  
+while count >= 1 do
+  puts count
+  count -= 1
+  puts "Happy New Year!" if count == 1
 end
