9c1f2ae1b57562068832bff16950e2236ded5089
Roman Turner <rn.turner@live.com>
2021-01-18 18:29:20 +0000

    Done.

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,1 +1,9 @@
-# Write your code here
+#This will countdown from 10 to 1, and then it will print "Happy New Year!"
+
+count = 10
+while count >= 1 do
+  puts count
+  count -= 1
+end
+
+puts "Happy New Year!"
