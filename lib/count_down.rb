1eb963a5d13511ae98e859f86a04be97889ded8c
Roman Turner <rn.turner@live.com>
2021-01-18 18:23:55 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,12 +1,4 @@
 # Write your code here
 =begin
-while (condition expression)
-do 
-body
-end
-As long as the condition of the expression is true, or truthy, the do..end block will run. Keep in mind, as an expression of the caondition expression can be quite rich using conditional operators && || ()
-Writing an infinite loop:
-while true do 
-puts "Say tis forever"
-end
+using mutating assignment operators
 =end
